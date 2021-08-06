ec89c974e8bb07a34d10b06db7cfc1ec804fe67c
yordy aquiles pena espinosa <serforin@gmail.com>
2021-08-05 20:45:27 -0400

    pcpios--01
    este es el primer commit del proyecto

--- calcu/AppDelegate.swift
+++ calcu/AppDelegate.swift
@@ -10,9 +10,12 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        //setUp
+        setUpView()
         // Override point for customization after application launch.
         return true
     }
@@ -29,6 +32,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    //Yordy: --Private methods
+    
+    private func setUpView(){
+        
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
     }
 
 
---
+++ calcu/view/Home/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  calcu
+//
+//  Created by yordy on 5/8/21.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    //MARK: Initialization
+    init(<#parameters#>) {
+        <#statements#>
+    }
+    //MARK: Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
